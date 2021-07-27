139a5722dfbf47e7208f9723676b5a9c8076d6ca
MohammadAl-khatib <alk20489@gmail.com>
2021-07-27 16:22:05 +0300

    2 new gussing games and some styling

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Mohammad AlKhatib Story
 
-This site tells the story of Mohammad, asks some questions about his life, and gives contact info.
+This site tells the story of Mohammad, asks some questions about his life, and gives contact info. Best travel detinations were added, and edited experience style.
--- js/app.js
+++ js/app.js
@@ -2,16 +2,19 @@
 
 var name = prompt('Hello, what is your name?');
 alert('Hi '+ name + ', welcome to my website, would you please answer my questions, just to get to know me');
+let counter = 0;
 
 let hobby = (prompt('Do I love swimming?')).toLowerCase();
 if(hobby=="yes"||hobby=="no"||hobby=="y"||hobby=="n"){
 switch (hobby){
     case 'yes':
         alert("ofcourse I love, who doesn't");
+        counter++;
         // console.log('you are correct');
         break;
     case 'y':
             alert("ofcourse I love, who doesn't");
+            counter++;
             // console.log('you are correct');
             break;
     default:
@@ -26,10 +29,12 @@ if(favDessert=="yes"||favDessert=="no"||favDessert=="y"||favDessert=="n"){
 switch (favDessert){
     case 'yes':
         alert("No, I like kunafa more, but cake is good too");
+        counter++;
         // console.log('you are correct');
         break;
     case 'y':
             alert("No, I like kunafa more, but cake is good too");
+            counter++;
             // console.log('you are correct');
             break;
     default:
@@ -42,10 +47,12 @@ if(dream=="yes"||dream=="no"||dream=="y"||dream=="n"){
 switch (dream){
     case 'yes':
         alert("Thanks, I hope you all the best");
+        counter++;
         // console.log('you are correct');
         break;
     case 'y':
             alert("Thanks, I hope you all the best");
+            counter++;
             // console.log('you are correct');
             break;
     default:
@@ -58,10 +65,12 @@ if(favMovie=="yes"||favMovie=="no"||favMovie=="y"||favMovie=="n"){
 switch (favMovie){
     case 'yes':
         alert("Sorry, There Will Be Blood is much better");
+        counter++;
         // console.log('you are correct');
         break;
     case 'y':
             alert("Sorry, There Will Be Blood is much better");
+            counter++;
             // console.log('you are correct');
             break;
     default:
@@ -74,13 +83,83 @@ if(travel=="yes"||travel=="no"||travel=="y"||travel=="n"){
 switch (travel){
     case 'yes':
         alert("It will be my best day");
+        counter++;
         // console.log('you are correct');
         break;
     case 'y':
             alert("It will be my best day");
+            counter++;
             // console.log('you are correct');
             break;
     default:
             alert("You must be kidding, it must be great to see");
 }
 }else {alert("please, enter only yes, no, y, or n");}
+
+// lab03
+
+//game1
+alert("now we will play a game");
+let guessNumber = prompt("guess the secret number?");
+for (let i=0; i<3; i++)
+{
+if(guessNumber>4){
+alert("too high");
+guessNumber = prompt("guess again");
+}else if(guessNumber<4)
+{
+alert("too low");
+guessNumber = prompt("guess again");
+}else{
+    alert("congrats, you win");
+    counter++;
+    i=3;
+    // console.log('you are correct');
+ 
+}
+if (i==2 && guessNumber==4)
+{
+alert ("congrats, you win")
+// console.log('you are correct');
+counter++;
+}
+if (i==2 && guessNumber!=4)
+{
+alert ("you lose, the secret number is 4")
+}
+}
+
+// game2
+alert("lets play another game");
+let hobbies = ['swimming','reading','video games'];
+let guessHobby = prompt('what is my hobby?')
+
+for (let i=0; i<5; i++)
+{
+for (let x=0; x<hobbies.length;x++)
+{
+if (guessHobby===hobbies[x])
+{
+alert("you win");
+counter++;
+i=5;
+// console.log('you are correct');
+}
+}if (i<5) {
+guessHobby=prompt('try again');
+}
+if (i==4) {
+    for (let x=0; x<hobbies.length;x++)
+{
+if (guessHobby===hobbies[x])
+{
+alert("you win");
+counter++;
+i=5;
+// console.log('you are correct');
+}
+    }
+
+}
+}
+alert('my hobbies are swimming, reading, and video games'+' your total score is '+counter);
