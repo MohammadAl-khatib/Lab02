a07a45e629ec5ffddb048bc63659a82fdfc3f31c
MohammadAl-khatib <alk20489@gmail.com>
2021-07-27 09:25:55 +0300

    edited js file,added if statement

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var name = prompt('Hello, what is your name?');
 alert('Hi '+ name + ', welcome to my website, would you please answer my questions, just to get to know me');
 
 let hobby = (prompt('Do I love swimming?')).toLowerCase();
+if(hobby=="yes"||hobby=="no"||hobby=="y"||hobby=="n"){
 switch (hobby){
     case 'yes':
         alert("ofcourse I love, who doesn't");
@@ -16,8 +17,12 @@ switch (hobby){
     default:
             alert("actually, I do like to swim");
 }
+} else{
+    alert("please, enter only yes, no, y, or n");
+}
 
 let favDessert = (prompt('Is cake my favourit dessert?').toLowerCase());
+if(favDessert=="yes"||favDessert=="no"||favDessert=="y"||favDessert=="n"){
 switch (favDessert){
     case 'yes':
         alert("No, I like kunafa more, but cake is good too");
@@ -30,8 +35,10 @@ switch (favDessert){
     default:
             alert("You are right, I like kunafa more");
 }
+} else {alert("please, enter only yes, no, y, or n");}
 
 let dream = (prompt('Am i going to become a professional programmer?').toLowerCase());
+if(dream=="yes"||dream=="no"||dream=="y"||dream=="n"){
 switch (dream){
     case 'yes':
         alert("Thanks, I hope you all the best");
@@ -44,8 +51,10 @@ switch (dream){
     default:
             alert("What you recommend to bocme one, you can tell me on alk20489@gmail.com");
 }
+}else {alert("please, enter only yes, no, y, or n");}
 
 let favMovie = (prompt('Is The Lord of The Rings my favourit movie?').toLowerCase());
+if(favMovie=="yes"||favMovie=="no"||favMovie=="y"||favMovie=="n"){
 switch (favMovie){
     case 'yes':
         alert("Sorry, There Will Be Blood is much better");
@@ -58,8 +67,10 @@ switch (favMovie){
     default:
             alert("There Will Be Blood is my favourit");
 }
+}else {alert("please, enter only yes, no, y, or n");}
 
 let travel = (prompt('Do I wish to see the deep oceans?').toLowerCase());
+if(travel=="yes"||travel=="no"||travel=="y"||travel=="n"){
 switch (travel){
     case 'yes':
         alert("It will be my best day");
@@ -72,3 +83,4 @@ switch (travel){
     default:
             alert("You must be kidding, it must be great to see");
 }
+}else {alert("please, enter only yes, no, y, or n");}
