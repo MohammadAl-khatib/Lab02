378425bdd3584df87cb760b49d1348b3a27f9a19
MohammadAl-khatib <alk20489@gmail.com>
2021-07-28 18:20:39 +0300

    shorter code used

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 var name = prompt('Hello, what is your name?');
 alert('Hi '+ name + ', welcome to my website, would you please answer my questions, just to get to know me');
 let counter = 0;
+// Question #1
 function guessHobby(){
 let hobby = (prompt('Do I love swimming?')).toLowerCase();
 if(hobby=="yes"||hobby=="no"||hobby=="y"||hobby=="n"){
@@ -25,26 +26,27 @@ switch (hobby){
 }
 }
 
+//Question #2
 function guessDessert(){
 let favDessert = (prompt('Is cake my favourit dessert?').toLowerCase());
 if(favDessert=="yes"||favDessert=="no"||favDessert=="y"||favDessert=="n"){
 switch (favDessert){
     case 'yes':
         alert("No, I like kunafa more, but cake is good too");
-        counter++;
-        // console.log('you are correct');
         break;
     case 'y':
             alert("No, I like kunafa more, but cake is good too");
             counter++;
-            // console.log('you are correct');
             break;
     default:
             alert("You are right, I like kunafa more");
+            counter++;
+             // console.log('you are correct');
 }
 } else {alert("please, enter only yes, no, y, or n");}
 }
 
+//Question #3
 function guessDream(){
 let dream = (prompt('Am i going to become a professional programmer?').toLowerCase());
 if(dream=="yes"||dream=="no"||dream=="y"||dream=="n"){
@@ -65,26 +67,26 @@ switch (dream){
 }else {alert("please, enter only yes, no, y, or n");}
 }
 
+//Question #4
 function guessMovie(){
 let favMovie = (prompt('Is The Lord of The Rings my favourit movie?').toLowerCase());
 if(favMovie=="yes"||favMovie=="no"||favMovie=="y"||favMovie=="n"){
 switch (favMovie){
     case 'yes':
         alert("Sorry, There Will Be Blood is much better");
-        counter++;
-        // console.log('you are correct');
         break;
     case 'y':
             alert("Sorry, There Will Be Blood is much better");
-            counter++;
-            // console.log('you are correct');
             break;
     default:
             alert("There Will Be Blood is my favourit");
+            counter++;
+        // console.log('you are correct');
 }
 }else {alert("please, enter only yes, no, y, or n");}
 }
 
+//Question #5
 function guessDestination(){
 let travel = (prompt('Do I wish to see the deep oceans?').toLowerCase());
 if(travel=="yes"||travel=="no"||travel=="y"||travel=="n"){
@@ -105,79 +107,56 @@ switch (travel){
 }else {alert("please, enter only yes, no, y, or n");}
 }
 
-// lab03
-
+// Lab03
 //game1
 function guessNumber(){
 alert("now we will play a game");
 
-let guessNumber = Number (prompt("guess the secret number?"));
-for (let i=0; i<3; i++)
+for (let i=0; i<4; i++)
 {
-if(guessNumber>4){
-alert("too high");
-}else if(guessNumber<4)
-{
-alert("too low");
-}else if(guessNumber==4){
+ let guessNumber = Number (prompt("guess the secret number?"));
+ if(guessNumber>4){
+ alert("too high");
+ }else if(guessNumber<4)
+ {
+ alert("too low");
+ }else if(guessNumber==4){
     alert("congrats, you win");
     counter++;
-    break;
     // console.log('you are correct');
-}else{
-    alert('please enter a number')
-}
-guessNumber = prompt("guess again");
-if (i==2 && guessNumber===4)
-{
-alert ("congrats, you win")
-// console.log('you are correct');
-counter++;
-}
-if (i==2 && guessNumber!==4)
-{
+    break; 
+ }else{
+    alert('please only numbers allowed')
+ }
+if (i==3 && guessNumber!==4)
+ {
 alert ("you lose, the secret number is 4")
-
-}
+ }
 }
 }
 
 // game2
 function guessHobbies(){
 alert("lets play another game");
-
 let hobbies = ['swimming','reading','video games'];
+
+outerloop :for (let i=0; i<6; i++)
+{
 let guessHobby = prompt('what is my hobby?')
+ for (let x=0; x<hobbies.length;x++)
+ {
+  if (guessHobby===hobbies[x])
+  {
+  alert("you win");
+  counter++;
+  // console.log('you are correct');
+  break outerloop;
+  }
+ }
+}
 
-for (let i=0; i<5; i++)
-{
-for (let x=0; x<hobbies.length;x++)
-{
-if (guessHobby===hobbies[x])
-{
-alert("you win");
-counter++;
-i=5;
-// console.log('you are correct');
-}
-}if (i<5) {
-guessHobby=prompt('try again');
-}
-if (i==4) {
-    for (let x=0; x<hobbies.length;x++)
-{
-if (guessHobby===hobbies[x])
-{
-alert("you win");
-counter++;
-i=5;
-// console.log('you are correct');
-}
-    }
-
-}
-}
-alert('my hobbies are swimming, reading, and video games'+' your total score is '+counter);
+alert('my hobbies are swimming, reading, and video games');
+alert('your total score is '+counter);
 }
 
 guessHobby();
